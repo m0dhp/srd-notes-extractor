b46caa725b56d361058ef46ae9619013a567aaa1
rjmbenitez <rjmbenitez@hotmail.com>
2023-08-09 20:25:29 +0100

    Added file output and debug flag to program.  Completed README.

--- README.md
+++ README.md
@@ -17,12 +17,13 @@ Download and extract the repository into a directory.
 
 Change to the directory.  Create and activate a virtual environment:
 
-```bash
+```
 $ python3 -m venv venv
 $ source venv/bin/activate
 ```
 
 Install the required Python packages: 
+
 ```
 pip3 install -r requirements.txt
 ```
@@ -30,19 +31,35 @@ pip3 install -r requirements.txt
 
 ## Usage
 
-### Configuration
+Save the SRD file in `.xlsx` format and copy it into the program folder.
 
-### Operation
+Edit the program to set the SRD file name.  For example:
+```
+SRD_FILE_NAME = "SRD-Spreadsheet-2023-07-13_CRC_9B2DAC22.xlsx"
+```
 
-```commandline
+Run the program:
+
+```
 python3 extract.py
 ```
+The program writes three text files into the program folder:
+
+| File name                              | Contents |
+|----------------------------------------|---|
+| `notes-SRD-Spreadsheet ...`       | List of just the note numbers from the "Notes" tab |
+| `notes-md-SRD-Spreadsheet ...`    | Same list but each item is preceded by markdown for a tick box list |
+| `references-SRD-Spreadsheet ...`  | Sorted list of note numbers which are referenced one or more times in the "Routes" tab |
+
+For testing, set the debug flag in the program.  If `DEBUG = True`, the program output
+is sent to the console rather than written to file.
+
 
 ## History
 
 
 
-##### 0.1 (2023-08-??)
+##### 0.1 (2023-08-09)
 
 First working version.
 
--- extract.py
+++ extract.py
@@ -3,10 +3,8 @@ from openpyxl import load_workbook
 
 ################################################################
 
-PROGRAM_STRING = 'SRD Notes Extract'
-VERSION_STRING = 'Version 0.1'
-
-SRD_FILE_NAME = "srd.xlsx"
+SRD_FILE_NAME = 'srd.xlsx'
+DEBUG = False
 
 ################################################################
 
@@ -14,12 +12,12 @@ SRD_FILE_NAME = "srd.xlsx"
 def get_notes(wb, markdown=False):
     # Assumes each note in column 1 of "Notes" tab starts with a row with format "Note <nnn>"
     # Markdown argument adds GitHub tick box markdown to start of each note number
-    ws = wb["Notes"]
+    ws = wb['Notes']
     notes = []
     for row in ws.iter_rows(min_row=0, max_col=1):
         note_text = row[0].value
         try:
-            if note_text.startswith("Note "):
+            if note_text.startswith('Note '):
                 notes.append(f'{"- [ ] " if markdown else ""}{note_text[5:]}')
         except AttributeError:
             pass
@@ -29,25 +27,40 @@ def get_notes(wb, markdown=False):
 def get_references(wb):
     # Assumes "Remarks" column on "Routes" tab has format "Notes: nnn" or "Notes: <nnn> - <nnn> <etc>"
     # SRD spreadsheet must be saved in ".xlsx" file format
-    ws = wb["Routes"]
+    ws = wb['Routes']
     references = set()
     for row in ws.iter_rows(min_row=0, max_col=8):
         remarks = row[7].value
         try:
-            if remarks.startswith("Notes: "):
-                clean_remarks = remarks[7:].replace("-","")
+            if remarks.startswith('Notes: '):
+                clean_remarks = remarks[7:].replace('-','')
                 references.update([int(ref) for ref in clean_remarks.split()])
         except AttributeError:
             pass
     return(references)
 
 
+def write_list(fn, l):
+    with open(fn, 'w') as file:
+        file.writelines(str(line) + '\n' for line in l)
+
+
 if __name__ == '__main__':
+
+    file_stem = SRD_FILE_NAME.split('.')[0]
 
     wb = load_workbook(SRD_FILE_NAME, read_only=True, data_only=True, keep_vba=True)
 
-    print(*get_notes(wb), sep='\n')
-    print(*get_notes(wb, True), sep='\n')
-    print(*sorted(list(get_references(wb))), sep='\n')
+    if DEBUG:
+        print('\nNOTES')
+        print(*get_notes(wb), sep='\n')
+        print('\nNOTES MARKDOWN')
+        print(*get_notes(wb, True), sep='\n')
+        print('\nREFERENCES')
+        print(*sorted(list(get_references(wb))), sep='\n')
+    else:
+        write_list(f'notes-{file_stem}.txt', get_notes(wb))
+        write_list(f'notes-md-{file_stem}.txt', get_notes(wb, True))
+        write_list(f'references-{file_stem}.txt', sorted(list(get_references(wb))))
 
     wb.close()
