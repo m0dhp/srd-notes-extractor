0703f2a7c022d9bd1a8e8fc2bc4e8ccf450b1e21
rjmbenitez <rjmbenitez@hotmail.com>
2023-08-09 17:24:29 +0100

    Tidied up code. Added argument to control addition of markdown to notes list.

--- scrape.py
+++ scrape.py
@@ -6,49 +6,48 @@ from openpyxl import load_workbook
 PROGRAM_STRING = 'SRD Scraper'
 VERSION_STRING = 'Version 0.1'
 
+SRD_FILE_NAME = "srd.xlsx"
+
 ################################################################
 
 
-def get_notes():
+def get_notes(wb, markdown=False):
     # Assumes each note in column 1 of "Notes" tab starts with a row with format "Note <nnn>"
-    try:
-        fn = "srd.xlsx"
-        wb = load_workbook(fn, read_only=True, data_only=True, keep_vba=True)
-        ws = wb["Notes"]
-        notes = []
-        for row in ws.iter_rows(min_row=0, max_col=1):
-            raw_note = row[0].value
-            if raw_note and raw_note.startswith("Note "):
-                notes.append(f'- [ ] {raw_note[5:]}')
-        wb.close()
-        return(notes)
-    except:
-        raise
-        quit()
+    # Markdown argument adds GitHub tick box markdown to start of each note number
+    ws = wb["Notes"]
+    notes = []
+    for row in ws.iter_rows(min_row=0, max_col=1):
+        note_text = row[0].value
+        try:
+            if note_text.startswith("Note "):
+                notes.append(f'{"- [ ] " if markdown else ""}{note_text[5:]}')
+        except AttributeError:
+            pass
+    return(notes)
 
 
-def get_references_set():
+def get_references_set(wb):
     # Assumes "Remarks" column on "Routes" tab has format "Notes: nnn" or "Notes: <nnn> - <nnn> <etc>"
     # SRD spreadsheet must be saved in ".xlsx" file format
-    try:
-        fn = "srd.xlsx"
-        wb = load_workbook(fn, read_only=True, data_only=True, keep_vba=True)
-        ws = wb["Routes"]
-        references = set()
-        for row in ws.iter_rows(min_row=0, max_col=8):
-            raw_remarks = row[7].value
-            if raw_remarks and raw_remarks.startswith("Notes: "):
-                clean_references = raw_remarks[7:].replace("-","")
-                individual_references = [int(ref.strip()) for ref in clean_references.split()]
-                references.update(individual_references)
-        wb.close()
-        return(references)
-    except:
-        raise
-        quit()
+    ws = wb["Routes"]
+    references = set()
+    for row in ws.iter_rows(min_row=0, max_col=8):
+        remarks = row[7].value
+        try:
+            if remarks.startswith("Notes: "):
+                clean_remarks = remarks[7:].replace("-","")
+                references.update([int(ref) for ref in clean_remarks.split()])
+        except AttributeError:
+            pass
+    return(references)
 
 
 if __name__ == '__main__':
 
-    # print(*get_notes(), sep='\n')
-    print(*sorted(list(get_references_set())), sep='\n')
+    wb = load_workbook(SRD_FILE_NAME, read_only=True, data_only=True, keep_vba=True)
+
+    print(*get_notes(wb), sep='\n')
+    print(*get_notes(wb, True), sep='\n')
+    print(*sorted(list(get_references_set(wb))), sep='\n')
+
+    wb.close()
